28e758d8c7c2a3b0ee804ca278c37259a1843e14
arkadva <94359963+arkadva@users.noreply.github.com>
2023-05-01 21:21:50 +0300

    Update main.py

--- main.py
+++ main.py
@@ -6,59 +6,73 @@ from modules.space import Space
 
 import random 
 
-def transform_data(space, sample_space, model_data, ratio=0.8):
+def transform_data(space, sample_space, model_data, transform, ratio):
     space.clear_space()
     sample_space.clear_space()
     
     with open(model_data) as file:
-        lines = [line.rstrip() for line in file]
-        line_number = 0
-        random_exlusion = random.sample(range(2, len(lines) + 1), int((1 - ratio) * (len(lines) - 1)))
+        data = [transform(line.rstrip().replace(', ', ',').split(',')) for i, line in enumerate(file) if i > 0]
         
-        for line in lines:
-            line_number += 1
-            if line_number == 1:
-                continue
-
-            location, date, precipitation, temp_max, temp_min, wind, classifier = line.replace(' ', '').split(',')
-            point = Point([float(precipitation), float(temp_max), float(temp_min), float(wind)], classifier)
-            
-            if line_number in random_exlusion:
-                sample_space.add_point(point)
-            else:
-                space.add_point(point)
-
+        random.shuffle(data)
+        split_index = int(len(data) * ratio)
+        
+        for point in data[:split_index]:
+            space.add_point(point)
+        
+        for point in data[split_index:]:
+            sample_space.add_point(point)
 
 def evaluate(sample_space, classifier, param1=10, param2=True):
-    total = 0
     correct_predictions = 0
     for point in sample_space.points:
-        total += 1
         if classifier.classify(point, param1, param2) == point.classifier:
             correct_predictions += 1
-    return correct_predictions/total*100
+    return correct_predictions/len(sample_space.points)
 
 
 def main():
+    # adjust your parameters here
+    runs = 10
+    split_ratio = 0.5
+    file_name = "data/seattle-weather.csv"
+    
+    def transform(line):
+        """
+        This function transforms a list of items from a dataset into a Point object. 
+        The function assumes that the input data is in a specific order, and therefore 
+        extracts the necessary values based on their positions in the input list. If the input 
+        data needs to be normalized, this function can be modified to include normalization 
+        steps. 
+
+        Arguments:
+        line - A list of items containing data for a single observation. 
+
+        Returns:
+        A Point object representing the input data and a class label for the observation. The class label
+        type agnostic.
+        """
+        location, date, precipitation, temp_max, temp_min, wind, classifier = line
+        point = Point([float(precipitation), float(temp_max), float(temp_min), float(wind)], classifier)
+        return point
+
     space, sample_space = Space(), Space()
     
     knn = KNNClassifier(space)
     bayes = BayesClassifier(space)
     
-    runs = 10
     knn_sum, bayes_sum = 0, 0
     
     for i in range(runs):
-        transform_data(space, sample_space, "data/seattle-weather.csv", 0.5)
+        transform_data(space, sample_space, file_name, transform, split_ratio)
         res_bayes, res_knn = evaluate(sample_space, bayes), evaluate(sample_space, knn)
         
         knn_sum += res_knn
         bayes_sum += res_bayes
         
-        print(f'Bayes {res_bayes}%, KNN {res_knn}%')
+        print(f'GN Bayes {res_bayes}, KNN {res_knn}')
       
-    print(f'Bayes average {bayes_sum/runs}%')
-    print(f'KNN {knn_sum/runs}%')
+    print(f'GN Bayes average {bayes_sum/runs}')
+    print(f'KNN {knn_sum/runs}')
 
 if __name__ == "__main__":
     main()
